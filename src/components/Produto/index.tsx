d99e55be28352cce75013eb19d83586c90d99672
JairoTJ <Jairotjunior@hotmail.com>
2024-10-31 16:27:47 -0300

    fix: Correção na adição dos favoritos e a troca dos textos dos botões

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -3,9 +3,16 @@ import { adicionar } from '../../store/reducers/carrinho'
 import { favoritar } from '../../store/reducers/favoritos'
 
 import * as S from './styles'
-import { useGetProdutosQuery } from '../../services/api'
+
+export type ProdutoType = {
+  id: number
+  nome: string
+  imagem: string
+  preco: number
+}
 
 type Props = {
+  produto: ProdutoType
   estaNosFavoritos: boolean
 }
 
@@ -14,39 +21,34 @@ export const paraReal = (valor: number) =>
     valor
   )
 
-const ProdutoComponent = ({ estaNosFavoritos }: Props) => {
+const ProdutoComponent = ({ produto, estaNosFavoritos }: Props) => {
   const dispatch = useDispatch()
-  const { data: produtos, isLoading } = useGetProdutosQuery()
-
-  if (!produtos || isLoading) return <h2>Carregando...</h2>
 
   return (
     <>
-      {produtos.map((produto) => (
-        <S.Produto key={produto.id}>
-          <S.Capa>
-            <img src={produto.imagem} alt={produto.nome} />
-          </S.Capa>
-          <S.Titulo>{produto.nome}</S.Titulo>
-          <S.Prices>
-            <strong>{paraReal(produto.preco)}</strong>
-          </S.Prices>
-          <S.BtnComprar
-            onClick={() => dispatch(favoritar(produto))}
-            type="button"
-          >
-            {estaNosFavoritos
-              ? '- Remover dos favoritos'
-              : '+ Adicionar aos favoritos'}
-          </S.BtnComprar>
-          <S.BtnComprar
-            onClick={() => dispatch(adicionar(produto))}
-            type="button"
-          >
-            Adicionar ao carrinho
-          </S.BtnComprar>
-        </S.Produto>
-      ))}
+      <S.Produto key={produto.id}>
+        <S.Capa>
+          <img src={produto.imagem} alt={produto.nome} />
+        </S.Capa>
+        <S.Titulo>{produto.nome}</S.Titulo>
+        <S.Prices>
+          <strong>{paraReal(produto.preco)}</strong>
+        </S.Prices>
+        <S.BtnComprar
+          onClick={() => dispatch(favoritar(produto))}
+          type="button"
+        >
+          {estaNosFavoritos
+            ? '- Remover dos favoritos'
+            : '+ Adicionar aos favoritos'}
+        </S.BtnComprar>
+        <S.BtnComprar
+          onClick={() => dispatch(adicionar(produto))}
+          type="button"
+        >
+          Adicionar ao carrinho
+        </S.BtnComprar>
+      </S.Produto>
     </>
   )
 }
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux'
+
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { RootReducer } from '../store'
 
 import * as S from './styles'
-import { RootReducer } from '../store'
 
 type Props = {
   produtos: ProdutoType[]
@@ -15,23 +16,20 @@ const ProdutosComponent = ({ produtos }: Props) => {
   )
 
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
     const IdsDosFavoritos = itensFavoritos.map((f) => f.id)
-
-    return IdsDosFavoritos.includes(produtoId)
+    return IdsDosFavoritos.includes(produto.id)
   }
 
   return (
-    <>
-      <S.Produtos>
-        {produtos.map((produto) => (
-          <Produto
-            estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
-          />
-        ))}
-      </S.Produtos>
-    </>
+    <S.Produtos>
+      {produtos.map((produto) => (
+        <Produto
+          produto={produto}
+          estaNosFavoritos={produtoEstaNosFavoritos(produto)}
+          key={produto.id}
+        />
+      ))}
+    </S.Produtos>
   )
 }
 
